d7dc5ad9c65b6b5a627f727c04bdc75714702477
Yuchen Dai <silentdai@gmail.com>
2021-01-15 22:19:32 -0800

    add glog
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>

--- source/sslapp/main.cc
+++ source/sslapp/main.cc
@@ -1,19 +1,32 @@
-#include <iostream>
+#include "glog/logging.h"
 
 #include "external/boringssl/src/include/openssl/bio.h"
 #include "external/boringssl/src/include/openssl/err.h"
 #include "external/boringssl/src/include/openssl/ssl.h"
 
-/* Initializing OpenSSL */
+class SslOnce {
+public:
+  static void init() {
+    // TODO(lambdai): CRYPTO_library_init is thread-safe and reentrant.
+    static SslOnce once;
+    LOG(INFO) << "In " << __FUNCTION__;
+  }
+
+private:
+  SslOnce() {
+    // Needed if boring ssl is built with BORINGSSL_NO_STATIC_INITIALIZER.
+    CRYPTO_library_init();
+  }
+};
 
 int main(int argc, char **argv) {
   // init_openssl();
-
+  SslOnce::init();
   SSL_CTX *ctx = SSL_CTX_new(TLS_method());
 
   SSL *ssl = SSL_new(ctx);
 
-  std::cout << "Hello, boringssl " << ssl << std::endl;
+  LOG(INFO) << "Hello, boringssl " << ssl;
   // configure_context(ctx);
   // int sock = create_socket(4433);
 
