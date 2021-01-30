b4c6ab730a83eab838eff106532c6d53af347bb8
Yuchen Dai <silentdai@gmail.com>
2021-01-29 16:56:38 -0800

    more msg in script
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>

--- script/runcs.sh
+++ script/runcs.sh
@@ -1,5 +1,12 @@
+echo "kill running nc"
+killall nc
+
 # listen on port 9000
+echo "listening on port 9000"
 nc -l 9000 &
 sleep 1
+
 # connect to proxy at port 4443
+echo "openssl connect"
 openssl s_client -ciphersuites TLS_AES_128_GCM_SHA256 -msg -state -debug -security_debug_verbose -keylogfile key.log -connect localhost:4443
+
